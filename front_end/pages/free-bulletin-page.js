4983a7a59ad49bec74ac7b243b0a46e966bf782b
ckd0325 <ckd0325@gmail.com>
2022-01-12 06:10:09 +0900

    1/12 로그인 기능 구현, SPA 확장, CSS 일부 적용

--- front_end/pages/free-bulletin-page.js
+++
@@ -1,9 +0,0 @@
-export default function drawFreeBulletinPage() {
-    const $rootContent = document.querySelector(".root-content");
-    while ($rootContent.hasChildNodes()) {
-        $rootContent.removeChild($rootContent.firstChild);
-    }
-
-    $rootContent.innerHTML += `<input type="button" value="글쓰기" data-link="write-post" />`
-
-}
--- front_end/pages/login-page.js
+++ front_end/pages/login-page.js
@@ -8,5 +8,29 @@ export default function drawLoginPage() {
     const $loginSignUpBox = document.querySelector(".login-signup-box");
 
     // 로그인 버튼 클릭 이벤트 구현 예정!!
-    $loginSignUpBox.innerHTML += `<input id="write-id" type="text" placeholder="id를 입력하시오" style="height:40px"><input class="blue-button" type="button" value="회원가입" data-link="sign-up"/><input class="blue-button" type="button" value="로그인" data-link="구현 필요!!" />`;
+    $loginSignUpBox.innerHTML += `<input id="write-id" type="text" placeholder="id를 입력하시오" style="height:40px"><input class="blue-button" type="button" value="회원가입" data-link="sign-up"/><input class="blue-button" id="login" type="button" value="로그인" data-link="free-bulletin" />`;
+
+    const $loginButton = document.querySelector('#login');
+    $loginButton.addEventListener('click', () => {
+        login(document.getElementById("write-id").value)
+    });
+}
+
+const login = async(userId) => {
+    const id = userId;
+    let result;
+    console.log(`입력한 ID: ${id}`);
+    try {
+        const response = await axios.post("login", { id: id, }); // DB에 해당하는 id가 있으면 true 반환
+        result = response.data;
+    } catch (err) {
+        console.log(err);
+    }
+    if (result === true) {
+        const $logIcon = document.getElementById('login-button');
+        $logIcon.textContent = "Log out";
+        alert(`환영합니다 ${id}님!!`);
+    } else {
+        alert('존재하지 않는 ID입니다.');
+    }
 }
