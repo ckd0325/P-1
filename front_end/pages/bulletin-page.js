c9e7a8ac570199546ec467f8b701a252e6223cae
ckd0325 <ckd0325@gmail.com>
2022-01-13 06:23:48 +0900

    이슈 해결 및 서버 API 수정

--- front_end/pages/bulletin-page.js
+++ front_end/pages/bulletin-page.js
@@ -1,19 +1,24 @@
 import { currentBulletin } from "../index.js";
 
-export default function drawBulletinPage() {
+export default async function drawBulletinPage() {
     const $rootContent = document.querySelector(".root-content");
     while ($rootContent.hasChildNodes()) {
         $rootContent.removeChild($rootContent.firstChild);
     }
 
+    $rootContent.innerHTML += `<div class="bulletin-page-box"></div>`;
+    const $bulletinPageBox = document.querySelector(".bulletin-page-box");
+
     //게시글 이름과 '등록하기' 버튼 그리기
-    $rootContent.innerHTML += `<div class="bulletin-and-writing"></div>`;
+    $bulletinPageBox.innerHTML += `<div class="bulletin-and-writing"></div>`;
     const $bulletinAndWriting = document.querySelector(".bulletin-and-writing");
     $bulletinAndWriting.innerHTML += `<div id="bulletin-name">${currentBulletin}</div><div id="write-post" data-link="write-post">등록하기</div>`;
 
     //게시글 목록 그리기
-    $rootContent.innerHTML += `<div class="bulletin-list"></div>`;
+    $bulletinPageBox.innerHTML += `<table class="bulletin-list"></table>`;
+    const $bulletinList = document.querySelector(".bulletin-list");
+
 
     //게시글 목록 번호
-    $rootContent.innerHTML += `<div class="bulletin-list-number"></div>`;
+    $bulletinPageBox.innerHTML += `<div class="bulletin-list-number"></div>`;
 }
